ac598ab1c3043e31722463814b816392057a91d2
iman imire <33391276+Imire0812@users.noreply.github.com>
2018-02-12 02:33:20 +0300

    comm

--- main.js
+++ main.js
@@ -1,137 +1,210 @@
-var currentValue = "0"; //текущее значение калькулятора (отображается на дисплее)
-var savedValue = "0"; //промежуточное значение
-var signMinus = false; //true - число отрицательное
-var currentOperation = null; //символ текущей операции
-var resetOnNumberPressed = false; //если после нажатия кнопки "=" пользователь нажимает цифру, дисплей сбрасывается до 0
+function Calculator() {
  
-var highlightId = null; //id подсвеченного элемента
+    var currentValue = "0"; //текущее значение калькулятора (отображается на дисплее)
+    var savedValue = "0"; //промежуточное значение
+    var signMinus = false; //true - число отрицательное
+    var currentOperation = null; //символ текущей операции
+    var resetOnNumberPressed = false; //если после нажатия кнопки "=" пользователь нажимает цифру, дисплей сбрасывается до 0
  
-function display() { //вспомагательная функция, обновляет дисплей
-    document.getElementById("display").innerHTML = signMinus ? "-" + currentValue : currentValue;
-}
+    var highlightElement = null; //подсвеченный элемент
  
-function numPressed(num) { //при нажатии на цифровую кнопку
-    if (resetOnNumberPressed) { //если после нажатия кнопки "=" пользователь нажимает цифру, дисплей сбрасывается до 0
-        reset();
-        resetOnNumberPressed = false;
+    var rootElement = null; //Корневой жлемент калькулятора, на него вешаются обработчики событий, его возвращает калькулятор
+    var displayElement = null; //Дисплей калькулятора, часто используется в коде
+ 
+    //Возвращает корень калькулятора
+    function getElement() {
+        if (!rootElement)
+            buildCalendar(); //Если калькулятор еще не построен
+        return rootElement;
     }
  
-    //дописываем к текущему значению нажатую цифру
-    if (currentValue == "0")
-        currentValue = String(num);
-    else
-        currentValue += num;
-    display();
-}
+    //Создает DOM калькулятора
+    function buildCalendar() {
+        var table = $("<table>").addClass("calc");
  
-function reset() { //сброс
-    currentValue = "0";
-    savedValue = "0";
-    signMinus = false;
-    resetHighlight();
-    display();
-}
+        for (var i = 0; i < 6; i++) {
+            var tr = $("<tr>");
+            table.append(tr);
+        }
  
-function signChange() { //смена знака
-    if (currentValue == "0") //для 0 знак не меняется
-        return;
+        //вспомагательная функция, создает кноку, сокращяет код
+        function makeButton(text, className) {
+            return $("<button>").addClass(className).html(text);
+        }
  
-    signMinus = !signMinus; //смена знака
-    display();
-}
+        //Дисплей
+        var display = $("<textarea>").addClass("calc_display").attr("rows", "1").attr("disabled", "true").text("0");
+        table.find("tr:eq(0)").append($("<td colspan='5'>").append(display));
  
-function operation(op) { //операция + - / *
-    if (currentOperation != null) //если в памяти есть какая-то незавершенная операция
-        calculate(); //нужно ее выполнить и обновить значение
-    resetOnNumberPressed = false;
+        //Первый ряд кнопок
+        var row = table.find("tr:eq(1)");
+        row.append($("<td colspan='3'>").append(makeButton("C", "calc_clear").css("width", "100%")));
+        row.append($("<td>").append(makeButton("&plusmn", "calc_sign")));
+        row.append($("<td>").append(makeButton("&#x232B;", "calc_backspace")));
  
-    //запоминает текущее значение в промежуточный результат
-    savedValue = signMinus ? "-" + currentValue : currentValue;
-    currentValue = 0;
-    signMinus = false;
-    currentOperation = op;
-    display();
-}
+        //Второй ряд кнопок
+        row = table.find("tr:eq(2)");
+        row.append($("<td>").append(makeButton(7, "calc_numeric")));
+        row.append($("<td>").append(makeButton(8, "calc_numeric")));
+        row.append($("<td>").append(makeButton(9, "calc_numeric")));
+        row.append($("<td>").append(makeButton("&divide;", "calc_operation").val("/")));
+		row.append($("<td>").append(makeButton("sqrt", "calc_operat").val("sqrt")));
  
-function calculate() { //выполнить подсчет операции, вспомогательная функция
-    if (currentOperation == null) //операции нет
-        return;
+        //Третий ряд кнопок
+        row = table.find("tr:eq(3)");
+        row.append($("<td>").append(makeButton(4, "calc_numeric")));
+        row.append($("<td>").append(makeButton(5, "calc_numeric")));
+        row.append($("<td>").append(makeButton(6, "calc_numeric")));
+        row.append($("<td>").append(makeButton("&#10005;", "calc_operation").val("*")));
+		row.append($("<td>").append(makeButton("tg", "calc_operat").val("tg")));
  
-    currentValue = signMinus ? "-" + currentValue : currentValue;
-    currentValue = String(eval(savedValue + currentOperation + currentValue)); //вычисляет значение
+        //Четвертый ряд кнопок
+        row = table.find("tr:eq(4)");
+        row.append($("<td>").append(makeButton(1, "calc_numeric")));
+        row.append($("<td>").append(makeButton(2, "calc_numeric")));
+        row.append($("<td>").append(makeButton(3, "calc_numeric")));
+        row.append($("<td>").append(makeButton("&#8722;", "calc_operation").val("-")));
+		row.append($("<td>").append(makeButton("cos", "calc_operat").val("cos")));
  
-    currentOperation = null;
-    savedValue = 0;
-    signMinus = false;
-    resetOnNumberPressed = true;
+        //Пятый ряд кнопок
+        row = table.find("tr:eq(5)");
+        row.append($("<td colspan='2'>").append(makeButton(0, "calc_numeric").css("width", "100%")));
+        row.append($("<td>").append(makeButton("=", "calc_equals")));
+        row.append($("<td>").append(makeButton("+", "calc_operation").val("+")));
+		row.append($("<td>").append(makeButton("sin", "calc_operat").val("sin")));
+		      
+		
+		
+	
  
-    if (currentValue[0] == '-') { //если число отрицательное
-        currentValue = currentValue.substring(1);
-        signMinus = true; //ХРАНИМ ЗНАК ТОЛЬКО В ЭТОЙ ПЕРЕМЕННОЙ (чтобы избежать множества глупых ошибок)
+        //обработка событий калькулятора
+        table.click(function (event) {
+            var className = event.target.className; //будем различать тип элемента по классу
+            switch (className) {
+                case "calc_clear": //нажата кнопка С
+                    reset();
+                    break;
+                case "calc_sign": //нажата кнопка +-
+                    signChange();
+                    break;
+                case "calc_numeric": //нажата цифровая кнопка
+                    numPressed(+event.target.textContent);
+                    break;
+                case "calc_backspace": //нажата кнока стереть
+                    backspace();
+                    break;
+                case "calc_operation": //нажата кнопка с операцией
+                    operation(event.target.value);
+                    highlight($(event.target));
+                    break;
+				case "calc_operat":
+					d(event.target.value);
+					highlight($(event.target));
+                    break;
+                case "calc_equals": //нажата кнопка =
+                    calculate();
+            }
+        });
+ 
+        rootElement = table;
+        displayElement = display;
     }
  
-    resetHighlight(); //операция выполнена, сбрасываем подсветку операций
-    display();
-}
  
-function backspace() { //удалить символ
-    if (resetOnNumberPressed) { //если после нажатия кнопки "=" пользователь нажимает "стереть", дисплей сбрасывается до 0
-        reset();
-        return;
+    function display() { //вспомагательная функция, обновляет дисплей
+        displayElement.html(signMinus ? "-" + currentValue : currentValue);
     }
  
-    if (currentValue.length === 1) { //если введен только один символ
-        currentValue = "0"; //обнуляем
+    function numPressed(num) { //при нажатии на цифровую кнопку
+        if (resetOnNumberPressed) { //если после нажатия кнопки "=" пользователь нажимает цифру, дисплей сбрасывается до 0
+            reset();
+            resetOnNumberPressed = false;
+        }
+ 
+        //дописываем к текущему значению нажатую цифру
+        if (currentValue == "0")
+            currentValue = String(num);
+        else
+            currentValue += num;
+        display();
+    }
+ 
+    function reset() { //сброс
+        currentValue = "0";
+        savedValue = "0";
         signMinus = false;
-    } else
-        currentValue = currentValue.substring(0, currentValue.length - 1); //иначе стираем последний
-    display();
-}
+        resetHighlight();
+        display();
+    }
  
-function highlight(id) { //выделить кнопку с этим id в голубой цвет
-    resetHighlight(); //сначала убираем подсветку с другой кнопки
+    function signChange() { //смена знака
+        if (currentValue == "0") //для 0 знак не меняется
+            return;
  
-    document.getElementById(id).style.backgroundColor = "lightblue";
-    highlightId = id; //запоминает id подсвеченной кнопки
-}
+        signMinus = !signMinus; //смена знака
+        display();
+    }
  
-function resetHighlight() { //очистка подсветки, вспомогательная функция
-    if (highlightId != null)
-        document.getElementById(highlightId).style.backgroundColor = "";
-}
-
-
-/* function d(val){
-	if(val=="sin"){
-		var x=document.getelementbyid("d").value;
-		document.getelementbyid("d").value=math.sin(x);
-		}
-	if(val=="cos"){
-		var x=document.getelementbyid("d").value;
-			document.getelementbyid("d").value=math.cos(x);
-		} 
-	if(val=="envers"){
-		var x=document.getelementbyid("d").value;
-		document.getelementbyid("d").value=eval(1/x);
-		}
-	if(val=="sqrt"){
-		var x=document.getelementbyid("d").value;
-		document.getelementbyid("d").value=math.sqrt(x);
-		}
-}
-function c(val){
-	document.getelementbyid("d").value=val;
-	}
-function v(val){
-	document.getelementbyid("d").value+=val;
-	} 
-	function e(){ try {
-		c(eval(document.getelementbyid("d").value))
-		} catch(e){
-			c('error')
-					}}  */
-
-function d(val){
+    function operation(op) { //операция + - / *
+        if (currentOperation != null) //если в памяти есть какая-то незавершенная операция
+            calculate(); //нужно ее выполнить и обновить значение
+        resetOnNumberPressed = false;
+ 
+        //запоминает текущее значение в промежуточный результат
+        savedValue = signMinus ? "-" + currentValue : currentValue;
+        currentValue = 0;
+        signMinus = false;
+        currentOperation = op;
+        display();
+    }
+ 
+    function calculate() { //выполнить подсчет операции, вспомогательная функция
+        if (currentOperation == null) //операции нет
+            return;
+ 
+        currentValue = signMinus ? "-" + currentValue : currentValue;
+        currentValue = String(eval(savedValue + currentOperation + currentValue)); //вычисляет значение
+ 
+        currentOperation = null;
+        savedValue = 0;
+        signMinus = false;
+        resetOnNumberPressed = true;
+ 
+        if (currentValue[0] == '-') { //если число отрицательное
+            currentValue = currentValue.substring(1);
+            signMinus = true; //ХРАНИМ ЗНАК ТОЛЬКО В ЭТОЙ ПЕРЕМЕННОЙ (чтобы избежать множества глупых ошибок)
+        }
+ 
+        resetHighlight(); //операция выполнена, сбрасываем подсветку операций
+        display();
+    }
+ 
+    function backspace() { //удалить символ
+        if (resetOnNumberPressed) { //если после нажатия кнопки "=" пользователь нажимает "стереть", дисплей сбрасывается до 0
+            reset();
+            return;
+        }
+ 
+        if (currentValue.length === 1) { //если введен только один символ
+            currentValue = "0"; //обнуляем
+            signMinus = false;
+        } else
+            currentValue = currentValue.substring(0, currentValue.length - 1); //иначе стираем последний
+        display();
+    }
+ 
+    function highlight(element) { //выделить кнопку с этим id в голубой цвет
+        resetHighlight(); //сначала убираем подсветку с другой кнопки
+ 
+        element.css("backgroundColor", "lightblue");
+        highlightElement = element; //запоминает подсвеченную кнопку
+    }
+ 
+    function resetHighlight() { //очистка подсветки, вспомогательная функция
+        if (highlightElement)
+            highlightElement.css("backgroundColor", "");
+    }
+ function d(val){
 
 if(val=="sin")
 {
@@ -155,4 +228,9 @@ if(val=="sqrt")
     var x=document.getElementById("display").value;
     document.getElementById("display").value=Math.sqrt(x);
 }
+
 } 
+    //делаем функцию, возвращающую корневой элемент калькулятора, публичной
+    this.getElement = getElement;
+ 
+}
